cf596bbf0e4c60dbca0b9b0820154c27e23226b5
SathvikDixit <sathvikdixit21@gmail.com>
2024-10-10 21:19:07 +0530

    just learning some new stufff

--- Practice.java
+++ Practice.java
@@ -4,9 +4,21 @@ public class Practice {
         // int num1 = 5.8;
         // double num2 = num1;    IMPLICIT CONVERTION
 
-        double num1 = 5.8;
-        int num2 = (int)num1;       //EXPLICIT CONVERTIOIN
+        // double num1 = 5.8;
+        // int num2 = (int)num1;       //EXPLICIT CONVERTIOIN
 
-        System.out.println(num2);    
+        // int nums[] = {1,8,4,9,6,7,5,3};
+        // int n = nums.size();
+        // for(int i=0;i<n;i++) {
+        //     if (nums[i] % 2 == 0)
+        //     System.out.println(nums[i]);        //Did'nt get output
+        // }
+
+
+        int num1 = 16;
+        int num2 = 9;
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 % num2);
     }
 }
