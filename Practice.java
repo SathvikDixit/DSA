83c7edcf69b3a2a3ce1e1124547ff357a50d4733
SathvikDixit <sathvikdixit21@gmail.com>
2024-10-10 20:46:26 +0530

    this is a practice file

---
+++ Practice.java
@@ -0,0 +1,12 @@
+public class Practice {
+    public static void main(String[] args) {
+
+        // int num1 = 5.8;
+        // double num2 = num1;    IMPLICIT CONVERTION
+
+        double num1 = 5.8;
+        int num2 = (int)num1;       //EXPLICIT CONVERTIOIN
+
+        System.out.println(num2);    
+    }
+}
