723a19eab77aa3a40a93eef4fba6de6c62829d8e
SathvikDixit <sathvikdixit21@gmail.com>
2024-10-13 22:06:05 +0530

    qwrqw

--- oop/HelloWorld.java
+++ oop/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
         User youngUser = new User();
 
         youngUser.name = ("Sathvik Jr.");
-        youngUser.birthDay = LocalDate.parse("18/05/2035");
+        youngUser.birthDay = LocalDate.parse("2035-01-31");
 
         System.out.printf("%s was born back in %s",youngUser.name , youngUser.birthDay.toString());
 
