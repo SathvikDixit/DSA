094c34d8db5f60011e9532b4f2d401721c483773
SathvikDixit <sathvikdixit21@gmail.com>
2024-10-14 23:12:08 +0530

    pritter

--- oop/HelloWorld.java
+++ oop/HelloWorld.java
@@ -7,8 +7,9 @@ public class HelloWorld {
         User youngUser = new User();
 
         youngUser.name = ("Sathvik Jr.");
-        youngUser.birthDay = LocalDate.parse("2035-05-18");          // In LocalDate the date order should be in (year-month-date)
- 
-        System.out.printf("%s was born in %s",youngUser.name , youngUser.birthDay.toString());
-    }    
+        youngUser.birthDay = LocalDate.parse("2035-05-18"); // In LocalDate the date order should be in
+                                                            // (year-month-date)
+
+        System.out.printf("%s was born in %s", youngUser.name, youngUser.birthDay.toString());
+    }
 }
