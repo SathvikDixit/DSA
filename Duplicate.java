91869392cd1bef3439d9e5b4d7961a48cd1b98f0
SathvikDixit <sathvikdixit21@gmail.com>
2024-10-14 23:13:58 +0530

    pritter for duplicate for duplicate

--- Duplicate.java
+++ Duplicate.java
@@ -1,23 +1,25 @@
 import java.util.*;
+
 public class Duplicate {
-    public static void main(String[] args) {   
-        int arr[] = {1,1,2,2,2,3,3,5,5,7,8,9,9};
+    public static void main(String[] args) {
+        int arr[] = { 1, 1, 2, 2, 2, 3, 3, 5, 5, 7, 8, 9, 9 };
         int k = removeDuplicates(arr);
         System.out.println("The array after removing duplicate elements is ");
         for (int i = 0; i < k; i++) {
             System.out.print(arr[i] + " ");
         }
     }
+
     static int removeDuplicates(int[] arr) {
-        HashSet < Integer > set = new HashSet < > ();
+        HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < arr.length; i++) {
             set.add(arr[i]);
         }
-        int k = set.size();  
+        int k = set.size();
         int j = 0;
-        for (int x: set) {
+        for (int x : set) {
             arr[j++] = x;
         }
-        return k;  
+        return k;
     }
 }
