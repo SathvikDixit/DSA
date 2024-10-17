5c6e92789a06f1ad0b0e9418f4f600c0ed7cee93
SathvikDixit <sathvikdixit21@gmail.com>
2024-10-17 18:58:21 +0530

    treeeeeeeeeee

---
+++ SingleElement.java
@@ -0,0 +1,9 @@
+class SingleElement {
+    public int singleNumber(int[] nums) {
+        int number = 0;
+        for (int i = 0; i<nums.length; i++) {
+            number ^= nums[i];
+        }
+        return number;
+    }
+}
