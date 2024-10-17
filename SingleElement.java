8f7a2f8e4b3ef38aedc4841d9b4d8b909f62d93b
SathvikDixit <sathvikdixit21@gmail.com>
2024-10-17 19:04:41 +0530

    tttttttttttt

--- SingleElement.java
+++ SingleElement.java
@@ -1,9 +1,18 @@
-class SingleElement {
+public class SingleElement {
     public int singleNumber(int[] nums) {
         int number = 0;
-        for (int i = 0; i<nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             number ^= nums[i];
         }
         return number;
+    }
+
+    public static void main(String[] args) {
+        SingleElement singleElement = new SingleElement();
+        
+        // Example test case
+        int[] nums = {4, 1, 2, 1, 2};
+        int result = singleElement.singleNumber(nums);
+        System.out.println("The single number is: " + result);
     }
 }
