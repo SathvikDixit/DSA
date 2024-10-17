3bdee1f794ff8a3674b05c4cb10d71a7c8d8a024
SathvikDixit <sathvikdixit21@gmail.com>
2024-10-17 18:33:38 +0530

    qwerrqwer

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -1,36 +1,32 @@
 class MergeSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int midx = m - 1; // Pointer for the last element in nums1 that is part of the initial array
-        int nidx = n - 1; // Pointer for the last element in nums2
-        int right = m + n - 1; // Pointer for the last position in nums1 (combined array length)
+        int midx = m - 1;
+        int nidx = n - 1; 
+        int right = m + n - 1; 
 
-        // Loop until all elements from nums2 have been merged
         while (nidx >= 0) {
-            // If nums1 still has elements left and the current element in nums1 is greater than nums2
             if (midx >= 0 && nums1[midx] > nums2[nidx]) {
-                nums1[right] = nums1[midx]; // Move the larger element to the end of nums1
-                midx--; // Move left in nums1
+                nums1[right] = nums1[midx]; 
+                midx--; 
             } else {
-                nums1[right] = nums2[nidx]; // Move the element from nums2 to nums1
-                nidx--; // Move left in nums2
+                nums1[right] = nums2[nidx]; 
+                nidx--; 
             }
-            right--; // Move left in nums1
+            right--; 
         }        
     }
 
     public static void main(String[] args) {
         MergeSortedArray solution = new MergeSortedArray();
         
-        // Example: nums1 has space for elements from nums2
         int[] nums1 = {1, 2, 3, 0, 0, 0};
-        int m = 3; // Number of elements initially in nums1
+        int m = 3; 
         
         int[] nums2 = {2, 5, 6};
-        int n = 3; // Number of elements in nums2
+        int n = 3; 
 
         solution.merge(nums1, m, nums2, n);
 
-        // Print the result after merging
         for (int num : nums1) {
             System.out.print(num + " ");
         }
