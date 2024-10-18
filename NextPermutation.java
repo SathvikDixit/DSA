c94b9d023106961e0325e33d8e610e704480d6a1
SathvikDixit <sathvikdixit21@gmail.com>
2024-10-18 20:30:38 +0530

    asdfasdf

--- NextPermutation.java
+++ NextPermutation.java
@@ -1,3 +1,46 @@
-public class NextPermutation {
-    
+import java.util.Arrays;
+
+class NextPermutation {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        reverse(nums, i + 1, nums.length - 1);
+    }
+
+    public static void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        
+        NextPermutation nextPerm = new NextPermutation();
+        
+        System.out.println("Before: " + Arrays.toString(nums));
+        
+        nextPerm.nextPermutation(nums);
+        
+        System.out.println("After: " + Arrays.toString(nums));
+    }
 }
