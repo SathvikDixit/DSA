6ef1d7409bd4593ebfe15bf445fde53a49b5603b
SathvikDixit <sathvikdixit21@gmail.com>
2024-10-24 23:27:11 +0530

    ertyerty

---
+++ Single.java
@@ -0,0 +1,18 @@
+public class Single {
+    public int singleNumber(int[] nums) {
+        int number = 0;
+        for (int i = 0; i < nums.length; i++) {
+            number ^= nums[i];
+        }
+        return number;
+    }
+
+    public static void main(String[] args) {
+        SingleElement singleElement = new SingleElement();
+        
+        // Example test case
+        int[] nums = {4, 1, 2, 1, 2};
+        int result = singleElement.singleNumber(nums);
+        System.out.println("The single number is: " + result);
+    }
+}
