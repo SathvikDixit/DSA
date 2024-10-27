7b436df879bb40585101261e42203ebb1bb72779
SathvikDixit <sathvikdixit21@gmail.com>
2024-10-27 20:45:07 +0530

    rretet

--- TwoStringEqual.java
+++ TwoStringEqual.java
@@ -6,7 +6,7 @@ class TwoStringEqual {
     }
 
     // makeString is a healper method here
-    String makeString(String[] word) { 
+    String makeString(String[] word) {
         StringBuilder sb = new StringBuilder();
         for (String s : word) {
             sb.append(s);
@@ -16,18 +16,18 @@ class TwoStringEqual {
 
     public static void main(String[] args) {
         TwoStringEqual solution = new TwoStringEqual();
-        
+
         // Example test cases
-        String[] word1 = {"abc", "d", "efg"};
-        String[] word2 = {"abcd", "efg"};
-        System.out.println(solution.arrayStringsAreEqual(word1, word2)); 
-        
-        String[] word3 = {"abc", "d"};
-        String[] word4 = {"ab", "c", "d"};
-        System.out.println(solution.arrayStringsAreEqual(word3, word4)); 
-        
-        String[] word5 = {"a", "b", "c"};
-        String[] word6 = {"ab", "c"};
-        System.out.println(solution.arrayStringsAreEqual(word5, word6)); 
+        String[] word1 = { "abc", "d", "efg" };
+        String[] word2 = { "abcd", "efg" };
+        System.out.println(solution.arrayStringsAreEqual(word1, word2));
+
+        String[] word3 = { "abc", "d" };
+        String[] word4 = { "ab", "c", "d" };
+        System.out.println(solution.arrayStringsAreEqual(word3, word4));
+
+        String[] word5 = { "a", "b", "c" };
+        String[] word6 = { "ab", "c" };
+        System.out.println(solution.arrayStringsAreEqual(word5, word6));
     }
 }
