7f6951c7a54352fc9036022b0d1a5f5a14eaea55
SathvikDixit <sathvikdixit21@gmail.com>
2024-10-27 20:44:29 +0530

    tttyuyuyr

--- NumofSubArrays.java
+++ NumofSubArrays.java
@@ -1,25 +1,22 @@
 
 public class NumOfSubArrays {
     public int numOfSubarrays(int[] arr) {
-                long ans = 0;
-                long odd = 0; 
-                long even = 0;
-                for (int num : arr) {
-                    if (num % 2 == 0) {
-                        even ++;
-                    } else {
-                        long temp = even;
-                        even = odd;
-                        odd = temp;
-                        odd++;
-                    }
-                    ans += odd;
-                }
-                return (int)(ans % (Math.pow(10, 9) + 7));
+        long ans = 0;
+        long odd = 0;
+        long even = 0;
+        for (int num : arr) {
+            if (num % 2 == 0) {
+                even++;
+            } else {
+                long temp = even;
+                even = odd;
+                odd = temp;
+                odd++;
             }
-    
-    
-    
+            ans += odd;
+        }
+        return (int) (ans % (Math.pow(10, 9) + 7));
+    }
 
     // You can add a main method to test the code if needed
     public static void main(String[] args) {
