539623fb1162d4baf383383b4b29c59dca5d997b
SathvikDixit <sathvikdixit21@gmail.com>
2024-10-27 20:47:00 +0530

    TYTRE

--- Twosum.java
+++ Twosum.java
@@ -4,8 +4,7 @@ public class Twosum {
         ans[0] = ans[1] = -1;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                if (arr[i] + arr[j] == target) 
-                {
+                if (arr[i] + arr[j] == target) {
                     ans[0] = i;
                     ans[1] = j;
                     return ans;
