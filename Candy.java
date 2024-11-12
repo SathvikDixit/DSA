867da90dff4d30324b41b645cbf822f297b64b1b
SathvikDixit <sathvikdixit21@gmail.com>
2024-11-12 18:07:43 +0530

    completed

--- Candy.java
+++ Candy.java
@@ -14,6 +14,16 @@ class Candy {
             if (ratings[i] > ratings[i - 1])
             left[i] = left[i - 1]+1;
         }
-        
+
+        for (int i = n-2; i>= 0; i--) {
+            if (ratings[i] > ratings[i + 1])
+            right[i] = right[i+1] + 1;
+        }
+
+        for(int i =0; i < n; i++) {
+            candies += Math.max(left[i], right[i]);
+        }
+
+        return candies;
     }
 }
