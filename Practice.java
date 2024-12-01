19481267903e6d77dab55bb6ca16670c9f7cefae
SathvikDixit <sathvikdixit21@gmail.com>
2024-12-01 17:56:29 +0530

    THis is just me adding some commit's to the repo

--- Practice.java
+++ Practice.java
@@ -177,10 +177,6 @@ public class Practice {
 
 
 
-
-
-
-
     // SWITCH CASE STATEMENTS
         // Scanner calci = new Scanner(System.in);
         // System.out.print("Enter the first number: ");
