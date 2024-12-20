f3f10985bd00916c901a899791cca1e3dd09ff78
SathvikDixit <sathvikdixit21@gmail.com>
2024-12-20 20:30:03 +0530

    werrewfghuy

--- Practice.java
+++ Practice.java
@@ -138,11 +138,6 @@ public class Practice {
 
 
 
-
-
-
-
-
     // CONDITIONAL STATEMENTS
     //     Scanner calci = new Scanner(System.in);
     //     System.out.print("Enter the first number: ");
