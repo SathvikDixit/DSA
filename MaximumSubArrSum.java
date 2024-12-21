88d610611d7ba5071a71b8890ff8696c08570f72
SathvikDixit <sathvikdixit21@gmail.com>
2024-12-21 23:40:04 +0530

    asdasdf

--- MaximumSubArrSum.java
+++ MaximumSubArrSum.java
@@ -16,4 +16,11 @@ public class MaximumSubArrSum {
         return maxi;
     }
 
+    public static void main (String[] args) {
+        int[] arr = { -2,1,-3,4,-1,2,1,-5,4};
+        int n = arr.length;
+        
+        int maxSum = maxsubarray(arr,n);
+        System.out.println("Maximum subArray Sum = " +maxSum);
+    }
 }
