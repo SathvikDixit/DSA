19496ccc2210c8d88e938b545f4eba1cac3ff516
SathvikDixit <sathvikdixit21@gmail.com>
2024-12-21 22:13:49 +0530

    sdfasasdfsdf

--- ReversingArray.java
+++ ReversingArray.java
@@ -14,8 +14,14 @@ public class ReversingArray {
 
     public static void main (String[] args) {
         int[] arr = { 2 , 4 , 6 , 5 , 8 , 9 };
+
+        System.out.print("Old Array ");
+        for(int i = 0; i < arr.length; i++)
+        System.out.print(arr[i] + " ");
+        System.out.println("");
         revArray(arr);
 
+        System.out.print("New Array ");
         for (int i = 0; i < arr.length; i++)
         System.out.print(arr[i] + " ");
     }
