fb61d1e33c51ef2ab0aaae5ce42d3b8ec3470e39
SathvikDixit <sathvikdixit21@gmail.com>
2024-12-21 19:32:29 +0530

    sdfasdfa

--- MinAndMaxInArray.java
+++ MinAndMaxInArray.java
@@ -1,6 +1,6 @@
 
 public class MinAndMaxInArray {
-    public static int setmini (int[] A, int N) {
+   /*  public static int setmini (int[] A, int N) {
         int mini = Integer.MAX_VALUE;
         for (int i = 0; i < N; i++) {
             if (A[i] < mini){
@@ -18,13 +18,29 @@ public class MinAndMaxInArray {
             }
         }
         return max;
+    } */      //Space Complexity is n^2
+
+    public static int asdf (int[] A, int N){
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < N; i++) {
+            if(A[i] < min) {
+                min = A[i];
+            }
+            else if (A[i] > max){
+                max = A[i];
+            }  
+        }
+        return min;
+        // return max;
     }
 
     public static void main (String[] args) {
         int[] A = {5,7,3,9,2};
         int N = A.length;
 
-        System.out.println("Minimum Element: " +setmini(A, N));
-        System.out.println("Maximum Element: " +setmax(A, N));
+        System.out.println("Minimum Element: " +asdf(A,N));
+        System.out.println("Maximum Element: " +asdf(A,N));
     }
 }
