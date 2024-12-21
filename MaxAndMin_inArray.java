24df6bb743fcea8652dcdd9d1d763d51ebfe78d1
SathvikDixit <sathvikdixit21@gmail.com>
2024-12-21 18:27:57 +0530

    Sathvik Didit

--- MaxAndMin_inArray.java
+++ MaxAndMin_inArray.java
@@ -1,3 +1,24 @@
 public class MaxAndMin_inArray {
+    public static int setmini(int[] A, int N) {
+        int mini = Integer.MAX_VALUE;
+        for (int i = 0; i <= N; i++){
+            if(A[i] < mini) {
+                mini = A[i];
+            }
+        }
+        return mini;
+    }
+
+    public static int setmax (int[] A, int N) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i > max; i++) {
+            if (A[i] > max) {
+                max = A[i];
+            }
+        }
+        return max;
+    }
+
     
+
 }
