c9d75c66ff1c9480b0251bec63786753f4a998c4
SathvikDixit <sathvikdixit21@gmail.com>
2024-12-21 18:46:25 +0530

    found minimum

--- MinAndMaxInArray.java
+++ MinAndMaxInArray.java
@@ -1,5 +1,14 @@
 
 public class MinAndMaxInArray {
+    public static int setmini (int[] A, int N) {
+        int mini = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            if (A[i] < mini){
+                mini = A[i];
+            }
+        }
+        return mini;
+    }
 
 
 }
