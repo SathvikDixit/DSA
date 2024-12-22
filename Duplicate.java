9efa294a935ecc981ce698baa97e60820ad8552e
SathvikDixit <sathvikdixit21@gmail.com>
2024-12-22 15:23:15 +0530

    Changes in code

--- Duplicate.java
+++ Duplicate.java
@@ -1,25 +1,16 @@
-import java.util.*;
+import java.util.Arrays;
 
 public class Duplicate {
-    public static void main(String[] args) {
-        int arr[] = { 1, 1, 2, 2, 2, 3, 3, 5, 5, 7, 8, 9, 9 };
-        int k = removeDuplicates(arr);
-        System.out.println("The array after removing duplicate elements is ");
-        for (int i = 0; i < k; i++) {
-            System.out.print(arr[i] + " ");
+    static boolean checkDuplicate (int[] arr) {
+
+        int n = arr.length;
+
+        Arrays.sort(arr);
+        for (int i = 1; i < n; i++) {
+            if (arr[i] == arr[i - 1])
+                return true;
         }
+        return false;
     }
 
-    static int removeDuplicates(int[] arr) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int i = 0; i < arr.length; i++) {
-            set.add(arr[i]);
-        }
-        int k = set.size();
-        int j = 0;
-        for (int x : set) {
-            arr[j++] = x;
-        }
-        return k;
-    }
 }
