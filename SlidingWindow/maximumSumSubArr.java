d6580a9fbc3297b57a7219de7876ca3d15791fa4
SathvikDixit <sathvikdixit21@gmail.com>
2025-09-09 20:34:02 +0530

    Complited optimal solution

--- SlidingWindow/maximumSumSubArr.java
+++ SlidingWindow/maximumSumSubArr.java
@@ -1,4 +1,6 @@
 
+/*
+ 
 public class maximumSumSubArr {
     public static int maxSumSubArr(int[] arr, int k) {
         int n = arr.length;
@@ -19,4 +21,35 @@ public class maximumSumSubArr {
         int soln = maxSumSubArr(arr, k);
         System.out.println(soln);
     }
+}
+
+ */                 // Tc = O(n*k)
+
+//Optimal Solution
+
+public class maximumSumSubArr {
+
+    public static int maxSumSubArr(int[] arr, int k) {
+        int n = arr.length;
+        
+        int windowSum = 0;
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+
+        int maxSum = 0;
+        for (int i = k; i<n; i++) {
+            windowSum += arr[i] - arr[i - k];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        return maxSum;
+    }
+    public static void main(String[] args) {
+        int[] arr = {100, 200, 300, 400};
+        int k = 2;
+        int soln = maxSumSubArr(arr, k);
+        System.out.println(soln);
+    }
+    
 }
