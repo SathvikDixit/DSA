a1739d2167e4076122364379b9ab15243538a0f7
SathvikDixit <sathvikdixit21@gmail.com>
2025-09-09 20:17:51 +0530

    sliding window 1st problem Maximum sub array

--- SlidingWindow/maximumSumSubArr.java
+++ SlidingWindow/maximumSumSubArr.java
@@ -1,5 +1,22 @@
-package SlidingWindow;
 
 public class maximumSumSubArr {
-    
+    public static int maxSumSubArr(int[] arr, int k) {
+        int n = arr.length;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i <= n-k; i++) {
+            int sum = 0;
+            for (int j = i; j < i+k; j++) {
+                sum += arr[j];
+            }
+            maxSum = Math.max(maxSum, sum);
+        }
+        return maxSum;
+    }
+    public static void main(String[] args) {
+        int[] arr = {100, 200, 300, 400};
+        int k = 2;
+        int soln = maxSumSubArr(arr, k);
+        System.out.println(soln);
+    }
 }
